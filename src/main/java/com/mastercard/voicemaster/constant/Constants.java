489ff938e59a6b7b00a16437c73095a6bbd473ce
shinde_h <harsh.shinde7@gmail.com>
2021-02-04 12:18:38 +0530

    Constant case changes

--- src/main/java/com/mastercard/voicemaster/constant/Constants.java
+++ src/main/java/com/mastercard/voicemaster/constant/Constants.java
@@ -2,9 +2,9 @@ package com.mastercard.voicemaster.constant;
 
 public class Constants {
 
-    public static final String WITHDRAW = "withdraw";
-    public static final String DEPOSIT = "deposit";
-    public static final String TRANSFER = "transfer";
+    public static final String WITHDRAW = "Withdraw";
+    public static final String DEPOSIT = "Deposit";
+    public static final String TRANSFER = "Transfer";
     public static final String WITHDRAW_DESCRIPTION = "Withdrawn from account";
     public static final String FOR = " for ";
     public static final String PAYMENT = " bill payment";
